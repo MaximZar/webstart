20335147f60ded1cf40cba55ebfe2eb899484957
Maxim <m.a.zadorkin@gmail.com>
2020-01-18 23:09:26 +0400

    Добавил модальное окно с помощью js

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+var modal = document.querySelector("#modal");
+var button = document.querySelector("#btn");
+var close = document.querySelector("#close");
+
+function closeModal() {
+    modal.classList.remove("modal_active");
+}
+
+button.addEventListener('click', function(){
+    modal.classList.add("modal_active");
+    setTimeout(closeModal, 5000);
+});
+
+close.addEventListener('click', function(){
+    closeModal();
+});
