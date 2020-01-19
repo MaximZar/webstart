e89f216e1e210a49658da9cf996daf9bf05d8601
Maxim <m.a.zadorkin@gmail.com>
2020-01-19 04:29:21 +0400

    Создал модальное окно через jQuery и сделал прокрутку страницы

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,36 @@
-var modal = document.querySelector("#modal");
-var button = document.querySelector("#btn");
-var close = document.querySelector("#close");
+// var modal = document.querySelector("#modal");
+// var button = document.querySelector("#btn");
+// var close = document.querySelector("#close");
 
-function closeModal() {
-    modal.classList.remove("modal_active");
-}
+// function closeModal() {
+//     modal.classList.remove("modal_active");
+// }
 
-button.addEventListener('click', function(){
-    modal.classList.add("modal_active");
-    setTimeout(closeModal, 5000);
-});
+// button.addEventListener('click', function(){
+//     modal.classList.add("modal_active");
+//     setTimeout(closeModal, 5000);
+// });
 
-close.addEventListener('click', function(){
-    closeModal();
+// close.addEventListener('click', function(){
+//     closeModal();
+// });
+
+$(function(){
+
+    var button = $('#btn');
+    var modal = $('#modal');
+    var close = $('#close');
+
+    function modalWindow() {
+        modal.toggleClass('modal_active');
+    }
+
+    button.on('click', modalWindow);
+    close.on('click', modalWindow);
+
+    $('#btnUp').on('click',function(){
+        var pos = 0; 
+        $('body,html').animate({scrollTop: pos}, 500);
+    });
+
 });
